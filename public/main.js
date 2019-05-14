c3f16d2903de05bded81e27b675282f702fb90cd
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-14 19:37:30 -0400

    stated background

--- public/main.js
+++ public/main.js
@@ -1,26 +1,26 @@
 const main = () => {}
 // when the button is clicked get api
 const getWeatherFromApi = () => {
-    // take input to api
-    let currentValue = document.querySelector('.city-zip').value
-    console.log(currentValue)
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + currentValue + '&appid=b986bcf3141fece7fea26687405f1183')
-      .then(resp => {
-        return resp.json()
+  // take input to api
+  let currentValue = document.querySelector('.city-zip').value
+  console.log(currentValue)
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + currentValue + '&appid=b986bcf3141fece7fea26687405f1183')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(weather => {
+      console.log({
+        weather
       })
-      .then(weather => {
-        console.log({
-          weather
-        })
-        const kelvinConvert = Math.ceil(weather.main.temp * (9 / 5) - 456.67)
-        document.querySelector('.temp').textContent = kelvinConvert
-        document.querySelector('.humidity').textContent = weather.main.humidity
-        document.querySelector('.clouds').textContent = weather.weather[0].description
-        document.querySelector('.city').textContent = weather.name
-      })
+      const kelvinConvert = Math.ceil(weather.main.temp * (9 / 5) - 456.67)
+      document.querySelector('.temp').textContent = kelvinConvert
+      document.querySelector('.humidity').textContent = weather.main.humidity
+      document.querySelector('.clouds').textContent = weather.weather[0].description
+      document.querySelector('.city').textContent = weather.name
+    })
 
+}
 
-
-    // get results
-    document.querySelector('.search').addEventListener('click', getWeatherFromApi)
-    document.addEventListener('DOMContentLoaded', main)
+// get results
+document.querySelector('.search').addEventListener('click', getWeatherFromApi)
+document.addEventListener('DOMContentLoaded', main)
