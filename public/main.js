85438178c14736955c587d7550112d49c7ff93cb
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-05-14 15:26:14 -0400

    city temp working

--- public/main.js
+++ public/main.js
@@ -1,7 +1,28 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const main = () => {}
+// when the button is clicked get api
+const getWeatherFromApi = () => {
+  // take input to api
+  let currentValue = document.querySelector('.city-zip').value
+  console.log(currentValue)
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + currentValue + '&appid=b986bcf3141fece7fea26687405f1183')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(weather => {
+      console.log({
+        weather
+      })
+      document.querySelector('.weather-results').textContent = weather.main.temp
+    })
+
+
+
+  // get results
+
 }
 
+
+
+
+document.querySelector('.search').addEventListener('click', getWeatherFromApi)
 document.addEventListener('DOMContentLoaded', main)
